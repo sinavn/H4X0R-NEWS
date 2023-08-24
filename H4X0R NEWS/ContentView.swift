9aa1d06d1da7aa32f3ef2e4c6adaf1140482e28f
sinavn <vosogh858sina@gmail.com>
2023-08-24 09:59:18 +0330

    Initial Commit

---
+++ H4X0R NEWS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4X0R NEWS
+//
+//  Created by SinaVN on 6/2/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R NEWS/H4X0R_NEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NEWSApp.swift
+//  H4X0R NEWS
+//
+//  Created by SinaVN on 6/2/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
